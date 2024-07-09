15cd94f27f7ade0485c1f8e8f785887cced90fc6
Alves0611 <g.alves0611@gmail.com>
2024-07-09 15:11:57 -0300

    feat: add get_connection method to DbConnectionHandler for retrieving the current database connection

--- python/src/models/settings/db_connection_handler.py
+++ python/src/models/settings/db_connection_handler.py
@@ -1,4 +1,5 @@
 import sqlite3 
+from sqlite3 import Connection
 
 class DbConnectionHandler: 
     def __init__(self) -> None:
@@ -8,3 +9,8 @@ class DbConnectionHandler:
     def connect(self) -> None: 
         conn = sqlite3.connect(self.__connection_string, check_same_thread=False)
         self.__conn = conn
+
+    def get_connection(self) -> Connection: 
+        return self.__conn 
+    
+db_connection_handler = DbConnectionHandler()
