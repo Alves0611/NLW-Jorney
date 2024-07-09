c044a89840ff9e4a49fb7b12e567ed6388612fe1
Alves0611 <g.alves0611@gmail.com>
2024-07-09 19:38:49 -0300

    feat: add method to update trip status in TripsRepository

--- python/src/models/repositories/trips_repository.py
+++ python/src/models/repositories/trips_repository.py
@@ -1,4 +1,4 @@
-# from typing import Dict
+from typing import Dict, Tuple
 from sqlite3 import Connection
 
 
@@ -6,15 +6,15 @@ class TripsRepository:
     def __init__(self, conn: Connection) -> None:
         self.__conn = conn
 
-    def create_trip(self, trips_infos: dict) -> None:
+    def create_trip(self, trips_infos: Dict) -> None:
         cursor = self.__conn.cursor()
         cursor.execute(
             """
                 INSERT INTO trips
-                  (id, destination, start_date, end_date, owner_name, owner_email)
+                    (id, destination, start_date, end_date, owner_name, owner_email)
                 VALUES
-                  (?, ?, ?, ?, ?, ?)
-""",
+                    (?, ?, ?, ?, ?, ?)
+            """,
             (
                 trips_infos["id"],
                 trips_infos["destination"],
@@ -26,8 +26,21 @@ class TripsRepository:
         )
         self.__conn.commit()
 
-    def find_trip_by_id(self, trip_id: str) -> None:
+    def find_trip_by_id(self, trip_id: str) -> Tuple:
         cursor = self.__conn.cursor()
         cursor.execute("""SELECT * FROM trips WHERE id = ?""", (trip_id,))
         trip = cursor.fetchone()
         return trip
+
+    def update_trip_status(self, trip_id: str) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            """
+                UPDATE trips
+                    SET status = 1
+                WHERE
+                    id = ?
+            """,
+            (trip_id,),
+        )
+        self.__conn.commit()
