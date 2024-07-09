28a2e35134e086d744d84c18af433f79fa41aaa7
Alves0611 <g.alves0611@gmail.com>
2024-07-09 19:35:48 -0300

    test: add test for finding a trip by ID in TripsRepository with pytest skip

--- python/src/models/repositories/trips_repository_test.py
+++ python/src/models/repositories/trips_repository_test.py
@@ -1,3 +1,4 @@
+import pytest
 import uuid
 from datetime import datetime, timedelta
 from .trips_repository import TripsRepository
@@ -5,18 +6,29 @@ from src.models.settings.db_connection_handler import db_connection_handler
 
 
 db_connection_handler.connect()
+trip_id = str(uuid.uuid4())
 
 
-def test_create_tip():
+@pytest.mark.skip(reason="interacao com o banco")
+def test_create_trip():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
 
     trips_infos = {
-        "id": str(uuid.uuid4),
+        "id": trip_id,
         "destination": "Osasco",
         "start_date": datetime.strptime("02-01-2024", "%d-%m-%Y"),
         "end_date": datetime.strptime("02-01-2024", "%d-%m-%Y") + timedelta(days=5),
-        "owner_name": "Gabriel",
-        "owner_email": "gabriel@gmail.com",
+        "owner_name": "Osvaldo",
+        "owner_email": "osvaldo@email.com"
     }
+
     trips_repository.create_trip(trips_infos)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_find_trip_by_id():
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+
+    trip = trips_repository.find_trip_by_id(trip_id)
+    print(trip)
