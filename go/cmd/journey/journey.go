8a5fcafde5abcc7aecee81e57d16e53944bc3013
Alves0611 <g.alves0611@gmail.com>
2024-07-19 22:15:24 -0300

    feat: add server startup and error handling with channels in main

--- go/cmd/journey/journey.go
+++ go/cmd/journey/journey.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"journey/internal/api"
-	"journey/internal/api/spec"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/phenpessoa/gutils/netutils/httputils"
+	"github.com/rocketseat-education/nlw-journey-go/internal/api"
+	"github.com/rocketseat-education/nlw-journey-go/internal/api/spec"
+	"github.com/rocketseat-education/nlw-journey-go/internal/mailer/mailpit"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -41,9 +43,9 @@ func run(ctx context.Context) error {
 	}
 
 	logger = logger.Named("journey_app")
-	defer func() { _ = logger.Sync }()
-	pool, err := pgxpool.New(ctx, fmt.Sprintf(
-		"user=%s password=%s host=%s port=%s dbname=%s",
+	defer func() { _ = logger.Sync() }()
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
 		os.Getenv("JOURNEY_DATABASE_USER"),
 		os.Getenv("JOURNEY_DATABASE_PASSWORD"),
 		os.Getenv("JOURNEY_DATABASE_HOST"),
@@ -58,8 +60,10 @@ func run(ctx context.Context) error {
 	if err := pool.Ping(ctx); err != nil {
 		return err
 	}
+
 	r := chi.NewMux()
 	r.Use(middleware.RequestID, middleware.Recoverer, httputils.ChiLogger(logger))
+
 	si := api.NewApi(
 		pool,
 		logger,
@@ -86,8 +90,23 @@ func run(ctx context.Context) error {
 		}
 	}()
 
+	errChan := make(chan error, 1)
+
+	go func() {
+		fmt.Println("server starting on:", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil {
+			errChan <- err
+		}
+	}()
+
 	select {
 	case <-ctx.Done():
 		return nil
+	case err := <-errChan:
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
 	}
+
+	return nil
 }
