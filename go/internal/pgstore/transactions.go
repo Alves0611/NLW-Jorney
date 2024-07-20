905b5a649af3a82124f3cde9d5599bd599ffc597
Alves0611 <g.alves0611@gmail.com>
2024-07-19 21:24:25 -0300

    chore: add transaction handling to CreateTrip in transactions.go

--- go/internal/pgstore/transactions.go
+++ go/internal/pgstore/transactions.go
@@ -2,7 +2,9 @@ package pgstore
 
 import (
 	"context"
+	"fmt"
 	"journey/internal/api/spec"
+
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -11,4 +13,9 @@ func (q *Queries) CreateTrip(
 	ctx context.Context,
 	pool *pgxpool.Pool,
 	params spec.CreateTripRequest,
-) (uuid.UUID, error) {}
+) (uuid.UUID, error) {
+	tx, err := pool.Begin(ctx)
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("pgstore: failed to begin tx for CreateTrip: %w", err)
+	}
+}
