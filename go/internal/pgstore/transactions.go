afd2b7f37c5f151ae93d4491f71ce3797944c8d8
Alves0611 <g.alves0611@gmail.com>
2024-07-19 22:18:17 -0300

    feat: add participant insertion logic to CreateTrip function in transactions.go

--- go/internal/pgstore/transactions.go
+++ go/internal/pgstore/transactions.go
@@ -3,24 +3,19 @@ package pgstore
 import (
 	"context"
 	"fmt"
-	"journey/internal/api/spec"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rocketseat-education/nlw-journey-go/internal/api/spec"
 )
 
-func (q *Queries) CreateTrip(
-	ctx context.Context,
-	pool *pgxpool.Pool,
-	params spec.CreateTripRequest,
-) (uuid.UUID, error) {
+func (q *Queries) CreateTrip(ctx context.Context, pool *pgxpool.Pool, params spec.CreateTripRequest) (uuid.UUID, error) {
 	tx, err := pool.Begin(ctx)
 	if err != nil {
-		return uuid.UUID{}, fmt.Errorf("pgstore: failed to begin trx for CreateTrip: %w", err)
+		return uuid.UUID{}, fmt.Errorf("pgstore: failed to begin tx for CreateTrip: %w", err)
 	}
-
-	defer tx.Rollback(ctx)
+	defer func() { _ = tx.Rollback(ctx) }()
 
 	qtx := q.WithTx(tx)
 	tripID, err := qtx.InsertTrip(ctx, InsertTripParams{
@@ -30,8 +25,25 @@ func (q *Queries) CreateTrip(
 		StartsAt:    pgtype.Timestamp{Valid: true, Time: params.StartsAt},
 		EndsAt:      pgtype.Timestamp{Valid: true, Time: params.EndsAt},
 	})
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("pgstore: failed to insert trip for CreateTrip: %w", err)
+	}
+
+	participants := make([]InviteParticipantsToTripParams, len(params.EmailsToInvite))
+	for i, eti := range params.EmailsToInvite {
+		participants[i] = InviteParticipantsToTripParams{
+			TripID: tripID,
+			Email:  string(eti),
+		}
+	}
+
+	if _, err := qtx.InviteParticipantsToTrip(ctx, participants); err != nil {
+		return uuid.UUID{}, fmt.Errorf("pgstore: failed to insert participants for CreateTrip: %w", err)
+	}
 
 	if err := tx.Commit(ctx); err != nil {
 		return uuid.UUID{}, fmt.Errorf("pgstore: failed to commit tx for CreateTrip: %w", err)
 	}
+
+	return tripID, nil
 }
