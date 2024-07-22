15837ecbc50fc06581db2a13ad81760f1337dd76
Alves0611 <g.alves0611@gmail.com>
2024-07-22 11:24:42 -0300

    test: add links_repository test with pytest

---
+++ python/src/models/repositories/links_repository_test.py
@@ -0,0 +1,22 @@
+import pytest 
+import uuid
+from .links_repository import LinksRepository
+from src.models.settings.db_connection_handler import db_connection_handler
+
+db_connection_handler.connect()
+
+trip_id = str(uuid.uuid4())
+link_id = str(uuid.uuid4())
+
+def test_registry_link(): 
+    conn = db_connection_handler.get_connection()
+    link_repository = LinksRepository(conn)
+
+    link_infos = {
+        "id": link_id,
+        "trip_id": trip_id,
+        "link": "somelink.com",
+        "title": "Hotel"
+    }
+
+    link_repository.registry_link(link_infos)
