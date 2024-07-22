9ce2c4454a8b86ec5a31381bcbb26763a83a0d2a
Alves0611 <g.alves0611@gmail.com>
2024-07-22 14:42:33 -0300

    feat: add necessary imports in trips_routes.py for trip creation functionality

--- python/src/main/routes/trips_routes.py
+++ python/src/main/routes/trips_routes.py
@@ -2,6 +2,11 @@ from flask import jsonify, Blueprint
 
 trips_routes_bp = Blueprint("trip_routes", __name__)
 
+from src.controllers.trip_creator import TripCreator
+from src.models.repositories.trips_repository import TripsRepository
+from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
+from src.models.settings.db_connection_handler import db_connection_handler
+
 
 @trips_routes_bp.route("/trips", methods=["POST"])
 def create_trip():
